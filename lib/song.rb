82e9f1d32ff6e15890aa967fefbc85905f5f591e
Kiri Um <kirirotha@gmail.com>
2020-07-17 23:39:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,9 +38,8 @@ class Song
 
   def self.find_or_create_by_name(name)
     song_in = Song.find_by_name(name)
-    if song_in == false
-      new_song = Song.create_by_name(name)
-      song_in = new_song
+    if song_in == nil
+      song_in = Song.create_by_name(name)
     end
     song_in
   end
