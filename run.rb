7ff7ca4e1c975b5163b75dd457f2c189210ab946
Kiri Um <kirirotha@gmail.com>
2020-07-18 00:06:31 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative 'lib/song.rb'
 
-song1 = Song.create
+#song1 = Song.create
 song2 = Song.create_by_name("Ho")
 song3 = Song.create_by_name("Ha")
 song4 = Song.create_by_name("Heee")
